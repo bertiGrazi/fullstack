3269295c585b9ff0335fc2481bc15cf3e2ea7136
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-11-01 09:11:44 -0300

    feat: start project

---
+++ app-todo/src/app/modules/home/home.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+
+//Modules
+import { CommonModule } from '@angular/common';
+
+//Componets
+import { HeaderComponent } from './components/header/header.component';
+import { TodoButtonDeleteAllComponent } from './components/todo-button-delete-all/todo-button-delete-all.component';
+import { TodoInputAddItensComponent } from './components/todo-input-add-itens/todo-input-add-itens.component';
+import { ToDoListComponent } from './components/to-do-list/to-do-list.component';
+
+//Pages
+import { HomeComponent } from './pages/home/home.component';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    TodoButtonDeleteAllComponent,
+    TodoInputAddItensComponent,
+    ToDoListComponent,
+    HomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
