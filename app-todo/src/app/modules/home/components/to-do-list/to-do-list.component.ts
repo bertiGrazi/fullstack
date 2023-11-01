122703b767767fc5572b6ba6d9aa96bc68826d24
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-11-01 14:13:03 -0300

    feat: submit itens in List Task

--- app-todo/src/app/modules/home/components/to-do-list/to-do-list.component.ts
+++ app-todo/src/app/modules/home/components/to-do-list/to-do-list.component.ts
@@ -10,13 +10,17 @@ import { TaskList } from '../../model/task-list';
 })
 export class ToDoListComponent implements OnInit {
 
-  public taskList: Array<TaskList> = [
-    {task: "Minha nova Task", checked: true},
-    {task: "Minha nova Task 2", checked: false},
-  ];
+  public taskList: Array<TaskList> = [];
   constructor() {}
 
   ngOnInit(): void {}
+
+  public setEmitTaskList(event: string) {
+    this.taskList.push({
+      task: event, 
+      checked: false
+    })
+  }
 
   public deleteItemList(event: number) {
     this.taskList.splice(event, 1)
