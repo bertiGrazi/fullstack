ca1e0121b8c6f1fd19e260b41a3352220965994e
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-11-07 16:22:47 -0300

    learning about angular forms

--- angular/src/app/forms/forms.component.ts
+++ angular/src/app/forms/forms.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.scss']
 })
-export class FormsComponent {
+export class FormsComponent implements OnInit{
+
+  public listFoods: Array<{food: string, preco: number}> = [
+    {food: "X-salada", preco: 23.90},
+    {food: "X-Bacon", preco: 23.90},
+    {food: "X-Egg", preco: 23.90},
+  ];
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  public submitForm(form: NgForm) {
+    if(form.valid) {
+      console.log(form.value)
+    }
+  }
 
 }
