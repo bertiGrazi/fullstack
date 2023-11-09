c6f9b2894d1af4a11240a08e034cc095f34b03fc
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-11-09 11:38:33 -0300

    queryParams

--- routes/src/app/pages/sobre/sobre.component.ts
+++ routes/src/app/pages/sobre/sobre.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-sobre',
   templateUrl: './sobre.component.html',
   styleUrls: ['./sobre.component.scss']
 })
-export class SobreComponent {
+export class SobreComponent implements OnInit{
+
+  constructor(private activateRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.activateRoute.params.subscribe(
+      res => console.log(res['id'], res['username'])
+    )
+
+    this.activateRoute.queryParams.subscribe(
+      res => console.log(res)
+    )
+  }
 
 }
