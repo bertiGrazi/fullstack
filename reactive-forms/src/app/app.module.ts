e549d4144c0502bd7d214366219264e6bf9eebcc
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-11-09 09:29:12 -0300

    learning about ReactiveFormsComponent

--- reactive-forms/src/app/app.module.ts
+++ reactive-forms/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactiveFormsComponent
   ],
   imports: [
     BrowserModule,
