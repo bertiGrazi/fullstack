3b83b263a3cb55badcc9a9bdbe8c16c903ce490b
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-11-23 10:04:20 -0300

    config themes style component

--- ignite_timer/src/App.tsx
+++ ignite_timer/src/App.tsx
@@ -1,14 +1,16 @@
+import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
+import { defaultTheme } from "./styles/theme/default";
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="primary"/>
       <Button variant="danger"/>
       <Button variant="secondary"/>
       <Button variant="sucess"/>
       <Button />
-    </>
+    </ThemeProvider>
   )
 }
 
