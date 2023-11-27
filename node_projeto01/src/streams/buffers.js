a0d307624f3a9d5309dad33370280678557a8522
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-11-27 19:37:52 -0300

    creating json middleware

---
+++ node_projeto01/src/streams/buffers.js
@@ -0,0 +1,6 @@
+// Representação de espaço na memória do computador para serem tratados
+// Salvar e ler da memória muito perfomática
+
+const buf = Buffer.from("hello")
+
+console.log(buf.toJSON())
