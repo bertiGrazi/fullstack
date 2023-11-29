e2c4eb905e7d4a24c8f1ffbc47ae363d530579ce
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-11-29 11:48:16 -0300

    create router file

--- node_projeto01/src/server.js
+++ node_projeto01/src/server.js
@@ -1,5 +1,4 @@
 import http from 'node:http'
-import { randomUUID } from 'node:crypto'
 import { Database } from './database/database.js'
 import { json } from './middlewares/json.js'
 
@@ -9,26 +8,8 @@ const server = http.createServer(async (req, res) => {
   const { method, url } = req
  
   await json(req, res)
+  
 
-  if (method === 'GET' && url === '/users') {
-    const users = database.select('users')
-
-    return res.end(JSON.stringify(users))
-  }
-
-  if (method === 'POST' && url === '/users') {
-    const { name, email } = req.body
-
-    const user = {
-      id: randomUUID(),
-      name,
-      email
-    }
-
-    database.insert('users', user)
-    
-    return res.writeHead(201).end()
-  }
   return res.writeHead(404).end()
 })
 
